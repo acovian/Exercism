bce8d26c59f26872394384ca0e5aaee41ac8075a
Alex Covian <alejandrocovian3@gmail.com>
2019-02-01 14:18:40 -0800

    added leap year program

---
+++ go/leap/leap.go
@@ -0,0 +1,16 @@
+// Package leap contains func IsLeapYear
+package leap
+
+// IsLeapYear checks to see if a given year is a leap year.
+func IsLeapYear(year int) bool {
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
+}
