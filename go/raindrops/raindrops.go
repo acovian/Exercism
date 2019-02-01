c4be0a6c0c2b8a9798eda9f9d78d16561b1446b3
Alex Covian <alejandrocovian3@gmail.com>
2019-02-01 14:28:16 -0800

    added raindrops exercise

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,22 @@
+package raindrops
+
+import "strconv"
+
+// Converts numbers to strings
+func Convert(number int) string {
+	raindrop := ""
+	if number%3 == 0 || number%5 == 0 || number%7 == 0 {
+		if number%3 == 0 {
+			raindrop += "Pling"
+		}
+		if number%5 == 0 {
+			raindrop += "Plang"
+		}
+		if number%7 == 0 {
+			raindrop += "Plong"
+		}
+		return raindrop
+	}
+	return strconv.Itoa(number)
+
+}
