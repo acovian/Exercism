b3d1cbe4b76c82cfdd6963a666dada20ccedbb50
Alex Covian <alejandrocovian3@gmail.com>
2019-03-01 10:56:15 -0800

    added python

---
+++ python/pangram/pangram.py
@@ -0,0 +1,4 @@
+def is_pangram(sentence):
+    # check if sentence contains non-alphabetic symbols.
+        # If sentence contains alphabetic symbols only, run following program:
+            # f
---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,5 @@
+def two_fer(name):
+    if name != "you" or name !="":
+        print("One for {}, one for me.".format(name) )
+    else:
+        print("One for you, one for me.")
